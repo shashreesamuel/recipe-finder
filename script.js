f21de77934858b625d343828f30f46466612c12f
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-05-29 09:21:26 -0400

    Currently working on the mobile version of the project specifically the event listeners in javascript regarding to the buttons

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 import data from "./data.json" assert { type: "json" };
-let btnSubmit = document.querySelector(".btn-submit");
+let searchButton = document.querySelector(".search-btn");
 let inputField = document.querySelector(".input-field");
 let result = document.querySelector(".result");
 
 // if the value specified by the user matches the name of a value in the data array.
 
-btnSubmit.addEventListener("click", () => {
+searchButton.addEventListener("click", () => {
   if (inputField.value == "") {
     alert("You need to specify the name of a recipe!!");
   }
@@ -15,3 +15,10 @@ btnSubmit.addEventListener("click", () => {
     }
   });
 });
+
+
+//TODO Adding an event listener to the search button
+
+searchButton.addEventListener('click', () => {
+  console.log("The button has been clicked")
+})
