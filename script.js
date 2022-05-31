113d08a21d81d04fca4252e2d1291a923158e50e
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-05-31 12:28:40 -0400

    Currently working on the retrieving the data from the api

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
-import data from "./data.json" assert { type: "json" };
-let inputField = document.querySelector(".input-field");
-let result = document.querySelector(".result");
-let errorMessage = document.querySelector(".error-message");
-let searchButton = document.querySelector(".search-btn");
-let unorderedList = document.querySelector("ul");
-let closeBtn = document.querySelector(".close-btn");
-// if the value specified by the user matches the name of a value in the data array.
+let containerInput = document.querySelector(".container__input");
+let recipeName = document.querySelector('.recipe-name')
+let recipeIngrdients = document.querySelector('.recipe-ingredients')
+let recipeMethod = document.querySelector('.recipe-method')
 
-searchButton.addEventListener("click", () => {
-  if (inputField.value == "") {
-    alert("You need to specify the name of a recipe!!");
-  }
-  data.map((val) => {
-    if (inputField.value == val.name) {
-      result.innerHTML = val.name;
-    } else {
-      errorMessage.innerHTML = `There is no recipe with the name of "${inputField.value}"`;
-      setTimeout(function () {
-        errorMessage.innerHTML = "";
-      }, 5000);
+async function processInput() {
+  if (containerInput.value == "") {
+    console.log("You need to specify a recipe to search for");
+  } else {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
+        "X-RapidAPI-Key": "dde48e2d97mshd0defcb83855200p195594jsn3d9c733fe6c4",
+      },
+    };
+
+    const data = await fetch(
+      `https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=${containerInput.value}&offset=1`,
+      options
+    )
+      .then((response) => response.json())
+      .catch(err => console.error(err));
+
+      data.map(({title, ingredients, instructions}) => {
+        recipeName.innerHTML = title;
+        recipeMethod.innerHTML = instructions
+        recipeIngrdients.innerHTML = ingredients;
+      })
     }
-  });
-});
-
-function removeItem() {
-  unorderedList.removeChild(result);
 }
 
-// ? Remove Item Function
-
-closeBtn.addEventListener("click", () => {
-  removeItem();
-});
-
-// ? Add Item Function
+processInput()
