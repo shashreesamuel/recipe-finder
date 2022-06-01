b42d6344c9bac8f20ed1c53f90cf00a7a8667a10
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-06-01 06:19:55 -0400

    Finished the concept of the recipe api in terms of fetching data and displaying it on the screen

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let containerInput = document.querySelector(".container__input");
-let recipeName = document.querySelector('.recipe-name')
-let recipeIngredients = document.querySelector('.recipe-ingredients')
-let recipeMethod = document.querySelector('.recipe-method')
+let recipeName = document.querySelector(".recipe-name");
+let recipeIngredients = document.querySelector(".recipe-ingredients");
+let recipeMethod = document.querySelector(".recipe-method");
 
 async function processInput() {
   if (containerInput.value == "") {
@@ -10,22 +10,25 @@ async function processInput() {
     const options = {
       method: "GET",
       headers: {
-        "X-RapidAPI-Host": "recipesapi2.p.rapidapi.com",
+        "X-RapidAPI-Host": "low-carb-recipes.p.rapidapi.com",
         "X-RapidAPI-Key": "dde48e2d97mshd0defcb83855200p195594jsn3d9c733fe6c4",
       },
     };
 
     const data = await fetch(
-      `https://recipesapi2.p.rapidapi.com/recipes/${containerInput.value}`,
+      `https://low-carb-recipes.p.rapidapi.com/search?name=${containerInput.value}`,
       options
     )
       .then((response) => response.json())
-      .then((response) => console.log(response))
+      // .then((response) => console.log(response))
       .catch((err) => console.error(err));
-        // recipeName.textContent = data.name;
-        // recipeMethod.innerHTML = data.instructions;
-        // recipeIngredients.innerHTML = data.ingredients;
-    }
+      data.map((val) => {
+        console.log(val.ingredients[0])
+        recipeName.innerHTML = val.name;
+        recipeMethod.innerHTML = val.description
+        recipeIngredients.innerHTML = val.ingredients[0].name;
+      })
+  }
 }
 
-processInput()
+processInput();
