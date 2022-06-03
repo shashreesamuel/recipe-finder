42d1230c753d920c1cce7bd1ee7b03470fcaeb1c
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-06-03 12:34:35 -0400

    Currently working on retrieving data from the api

--- script.js
+++ script.js
@@ -4,31 +4,50 @@ let recipeIngredients = document.querySelector(".recipe-ingredients");
 let recipeMethod = document.querySelector(".recipe-method");
 
 async function processInput() {
-  if (containerInput.value == "") {
-    console.log("You need to specify a recipe to search for");
-  } else {
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Host": "low-carb-recipes.p.rapidapi.com",
-        "X-RapidAPI-Key": "dde48e2d97mshd0defcb83855200p195594jsn3d9c733fe6c4",
-      },
-    };
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'recipify.p.rapidapi.com',
+		'X-RapidAPI-Key': 'dde48e2d97mshd0defcb83855200p195594jsn3d9c733fe6c4'
+	}
+};
 
-    const data = await fetch(
-      `https://low-carb-recipes.p.rapidapi.com/search?name=${containerInput.value}`,
-      options
-    )
-      .then((response) => response.json())
-      // .then((response) => console.log(response))
-      .catch((err) => console.error(err));
-      data.map((val) => {
-        console.log(val.ingredients[0])
-        recipeName.innerHTML = val.name;
-        recipeMethod.innerHTML = val.description
-        recipeIngredients.innerHTML = val.ingredients[0].name;
-      })
+fetch('https://recipify.p.rapidapi.com/today-recipe', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+      // console.log(data)
+    // data.map((val) => {
+    //   //! Creating the tags
+      // let recipeSet = document.createElement("div");
+      // recipeSet.classList.add('recipes')
+      // let recipeName = document.createElement("h1");
+      // recipeName.classList.add('recipe-name')
+      // let recipeIngredients = document.createElement("p");
+      // recipeIngredients.classList.add("recipe-ingredients");
+      // let recipeMethod = document.createElement("p");
+      // recipeMethod.classList.add("recipe-method");
+
+    //   /* 
+    //   Adds the heading and paragraph tags to the div element
+    //     <div>
+    //     <h1></h1>
+    //     <p></p>
+    //     <p></p>
+    //   </div>*/
+
+    //   recipeSet.appendChild(recipeName);
+    //   recipeSet.appendChild(recipeIngredients);
+    //   recipeSet.appendChild(recipeMethod);
+    //   recipeName.innerHTML = val.name;
+    //   recipeMethod.innerHTML = val.description
+    //   recipeIngredients.innerHTML = val.ingredients.map((n) => n.ingredient);
+
+    //   // Adds the list of recipes within the div with id of recipes
+
+    //   const recipes = document.getElementById("recipes");
+    //   recipes.appendChild(recipeSet);
+    // });
   }
-}
 
 processInput();
