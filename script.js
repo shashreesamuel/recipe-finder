c1587810bcf30a27319323abe8ed8adcbf60430d
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-06-03 21:32:03 -0400

    Fixed the api to retrieve all the data and display it using createElement() and ppendChild()

--- script.js
+++ script.js
@@ -5,49 +5,51 @@ let recipeMethod = document.querySelector(".recipe-method");
 
 async function processInput() {
   const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'recipify.p.rapidapi.com',
-		'X-RapidAPI-Key': 'dde48e2d97mshd0defcb83855200p195594jsn3d9c733fe6c4'
-	}
-};
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com",
+      "X-RapidAPI-Key": "dde48e2d97mshd0defcb83855200p195594jsn3d9c733fe6c4",
+    },
+  };
 
-fetch('https://recipify.p.rapidapi.com/today-recipe', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-      // console.log(data)
-    // data.map((val) => {
-    //   //! Creating the tags
-      // let recipeSet = document.createElement("div");
-      // recipeSet.classList.add('recipes')
-      // let recipeName = document.createElement("h1");
-      // recipeName.classList.add('recipe-name')
-      // let recipeIngredients = document.createElement("p");
-      // recipeIngredients.classList.add("recipe-ingredients");
-      // let recipeMethod = document.createElement("p");
-      // recipeMethod.classList.add("recipe-method");
+  const data = await fetch(
+    `https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=${containerInput.value}`,
+    options
+  )
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+      console.log(data)
+    data.map((val) => {
+      //! Creating the tags
+      let recipeSet = document.createElement("div");
+      recipeSet.classList.add('recipes')
+      let recipeName = document.createElement("h1");
+      recipeName.classList.add('recipe-name')
+      let recipeIngredients = document.createElement("p");
+      recipeIngredients.classList.add("recipe-ingredients");
+      let recipeMethod = document.createElement("p");
+      recipeMethod.classList.add("recipe-method");
 
-    //   /* 
-    //   Adds the heading and paragraph tags to the div element
-    //     <div>
-    //     <h1></h1>
-    //     <p></p>
-    //     <p></p>
-    //   </div>*/
+      /* 
+      Adds the heading and paragraph tags to the div element
+        <div>
+        <h1></h1>
+        <p></p>
+        <p></p>
+      </div>*/
 
-    //   recipeSet.appendChild(recipeName);
-    //   recipeSet.appendChild(recipeIngredients);
-    //   recipeSet.appendChild(recipeMethod);
-    //   recipeName.innerHTML = val.name;
-    //   recipeMethod.innerHTML = val.description
-    //   recipeIngredients.innerHTML = val.ingredients.map((n) => n.ingredient);
+      recipeSet.appendChild(recipeName);
+      recipeSet.appendChild(recipeIngredients);
+      recipeSet.appendChild(recipeMethod);
+      recipeName.innerHTML = val.title;
+      recipeIngredients.innerHTML = val.ingredients;
+      recipeMethod.innerHTML = val.instructions;
 
     //   // Adds the list of recipes within the div with id of recipes
 
-    //   const recipes = document.getElementById("recipes");
-    //   recipes.appendChild(recipeSet);
-    // });
+      const recipes = document.getElementById("recipes");
+      recipes.appendChild(recipeSet);
+    });
   }
 
 processInput();
